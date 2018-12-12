3f0c8de9529531d30b9eb7b9e5acb4d7fb5da5d4
Joseph Lalley <josephyzyr@gmail.com>
2018-12-12 20:41:21 +0000

    commit

--- src/components/trades/Index.js
+++ src/components/trades/Index.js
@@ -10,8 +10,7 @@ class TradeIndex extends React.Component {
 
   componentDidMount() {
     axios.all([
-      axios.get('/api/trades'),
-      axios.get('https://chasing-coins.com/api/v1/top-coins/20')
+      axios.get('/api/trades')
     ])
       .then(axios.spread((localTrades, externalCoinData) => {
         this.setState({ trades: localTrades.data });
@@ -36,16 +35,38 @@ class TradeIndex extends React.Component {
         <div className="portfolio-container">
           <div className="portfolio-header">
           </div>
-          {this.state.trades && this.state.trades.map(
-            (trade, i) =>
-              <Link to={`/trades/${trade._id}`}  key={i}>
-                <div>
-                  <p >{trade.coinName}</p>
-                  <p>{trade.transactionTotal}</p>
-                </div>
-              </Link>
-          )}
+          <div className="pa4">
+            <div className="overflow-auto">
+              <table className="f6 w-100 mw8 center" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white">Coin</th>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white">Coin Name</th>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white">Amount</th>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white">Value</th>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white">ID</th>
+                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white"></th>
+                  </tr>
+                </thead>
+                <tbody className="lh-copy">
+                  {this.state.trades && this.state.trades.map(
+                    (trade, i) =>
+                    <tr>
+                      <td className="pv3 pr3 bb b--black-20">{trade.symbol}</td>
+                      <td className="pv3 pr3 bb b--black-20">{trade.coinName}</td>
+                      <td className="pv3 pr3 bb b--black-20">{trade.transactionTotal}</td>
+                      <td className="pv3 pr3 bb b--black-20"></td>
+                      <td className="pv3 pr3 bb b--black-20">{trade._id}</td>
+                      <td className="pv3 pr3 bb b--black-20"><Link to={`/trades/${trade._id}`}  key={i}>View Trade</Link></td>
+
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+
       </div>
     );
   }
