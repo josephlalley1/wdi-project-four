cb7bb974abf62b1490f032c7f5fb31bf233bfb99
Joseph Lalley <josephyzyr@gmail.com>
2018-12-12 16:53:27 +0000

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <div className="homepage-container">
-      </div>
+      <h1>Welcome to the homepage</h1>
     </div>
   );
 }
